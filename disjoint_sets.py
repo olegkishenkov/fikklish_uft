a2b71b3e26d65c4deefbd7e09341a5a6d4d75b5d
oleg <oleg_kishenkov@mail.ru>
2024-06-12 02:02:58 +0300

    add logging, use multiprocessing Array for shared memory, add basic test cases

--- disjoint_sets.py
+++ disjoint_sets.py
@@ -1,7 +1,10 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, Array
+import logging
+from datetime import datetime
+logger = logging.getLogger(__name__)
+
 
 def make_set(i):
-    global labels, ranks
     labels[i] = i
     ranks[i] = 0
 
@@ -11,29 +14,35 @@ def find_set(i):
     return labels[i]
 
 def union(i, j):
+    logger.info(f'union {i} {j} {find_set(i)} {find_set(j)}')
     if ranks[i] < ranks[j]:
         labels[find_set(i)] = find_set(j)
     elif ranks[i] > ranks[j]:
         labels[find_set(j)] = find_set(i)
     else:
         labels[find_set(max(i, j))] = find_set(min(i, j))
+        logger.info(f'union  {list(labels)} {id(labels)}')
         ranks[min(i, j)] += 1
 
 def process_internal(start, end):
+    logger.info(f'process internal {start} {end}')
     for i in range(start, end):
-        for j in range(len(edges[i])):
+        for j in edges[i]:
             if j < i and start <= j < end and find_set(i) != find_set(j):
+                logger.info(f'process internal  {i} {j} {find_set(i)} {find_set(j)}')
                 union(i, j)
+
 
 def process_external():
-    global edges
     for i in range(n):
-        for j in range(len(edges[i])):
+        for j in edges[i]:
             if j < i and i // part_size != j // part_size and find_set(i) != find_set(j):
+                logger.info(f'process external {i} {j} {find_set(i)} {find_set(j)}')
                 union(i, j)
 
 
-labels: list[int]
+# labels: list[int]
+lables: Array
 ranks: list[int]
 processes_n: int = 4
 n: int
@@ -41,14 +50,14 @@ part_size: int
 edges: list[list[int]]
 
 
-
 def connected_components(edges_: list[list[int]]):
-    global labels, ranks, processes_n, n, part_size, edges
+    global edges, labels, ranks, n, part_size
     edges = edges_
     n = len(edges)
-    labels = [None] * n
+    # labels = [None] * n
+    labels = Array('i', [-1] * n)
     ranks = [None] * n
-    part_size = n // processes_n + 1
+    part_size = n // processes_n
 
     for i in range(n):
         make_set(i)
@@ -56,9 +65,10 @@ def connected_components(edges_: list[list[int]]):
     with Pool(processes_n) as p:
         p.starmap(
             process_internal,
-            ((part_size * i, min(part_size * (i + 1), n)) for i in range(processes_n)),
+            [(part_size * i, part_size * (i + 1)) for i in range(processes_n-1)] + [(part_size * (processes_n - 1), n)],
         )
 
+    logger.info(f'{list(labels)}, {id(labels)}')
     process_external()
 
-    return labels
+    return list(labels)
--- test_disjoint_sets.py
+++ test_disjoint_sets.py
@@ -1,15 +1,29 @@
+import logging
 from disjoint_sets import connected_components
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename='disjoint_sets.log',
+    level=logging.INFO,
+    force=True,
+    format='%(asctime)s - %(module)s - %(levelname)s - %(message)s',
+    filemode='w'
+)
 
 
 def test_two_connected_vertices():
     assert connected_components([[1], [0]]) == [0, 0]
 
-
 def test_two_and_one_vertices():
-    connected_components([[1], [0], []]) == [0, 0, 2]
+    assert connected_components([[1], [0], []]) == [0, 0, 2]
 
 def test_triangle():
-    connected_components([[1, 2], [0, 2], [0, 1]]) == [0, 0, 0]
+    assert connected_components([[1, 2], [0, 2], [0, 1]]) == [0, 0, 0]
 
 def test_triangle_unsorted():
-    connected_components([[1, 2], [2, 0], [0, 1]]) == [0, 0, 0]
+    assert connected_components([[1, 2], [2, 0], [0, 1]]) == [0, 0, 0]
+
+def test_four_triangles():
+    assert connected_components(
+        [[1, 2], [0, 2], [0, 1], [4, 5], [3, 5], [3, 5], [7, 8], [6, 8], [6, 7]]
+    ) == [0, 0, 0, 3, 3, 3, 6, 6, 6]
